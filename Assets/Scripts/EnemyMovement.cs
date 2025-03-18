123f89cd57078b3eb23bbaad74555cfff3fc06ff
Sunvaar45 <is.kalay1@gmail.com>
2025-03-18 20:39:11 +0300

    created the barebones enemy spawner

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform player;
+
+    public float moveSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null) return;
+
+        // oyuncu ve düşman pozisyonlarını değişkene aktar
+        Vector3 playerPos = player.position;
+        Vector3 enemyPos = transform.position;
+
+        Vector3 direction = (playerPos - enemyPos).normalized; // yönü oyuncunun pozisyonundan düşmanın pozisyonunu çıkartarak bul ve genişliği sabit tut
+        enemyPos += direction * moveSpeed * Time.deltaTime; // bulunan yönde düşmanı hareket ettir
+        transform.position = enemyPos;
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public Transform player;
+
+    public float spawnRate = 2f;
+
+    public float spawnDistance = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // SpawnEnenmy metodu oyun başladıktan 1f sonra bir kere çalışır sonra her spawnRate sürsinde birdaha çalışır
+        InvokeRepeating(nameof(SpawnEnemy), 1f, spawnRate); 
+    }
+
+    // Update is called once per frame
+    void SpawnEnemy()
+    {
+        if (player == null) return;
+
+        Vector2 spawnPosition = (Vector2)player.position + Random.insideUnitCircle.normalized * spawnDistance; // düşmanın doğacağı pozisyonu rastgele seç
+
+        GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity); // düşmanı seçilen pozisyonda çağır
+
+        enemy.GetComponent<EnemyMovement>().player = player; // çağırılan düşmandaki hareket scriptine oyuncu nesnesini aktar
+    }
+}
