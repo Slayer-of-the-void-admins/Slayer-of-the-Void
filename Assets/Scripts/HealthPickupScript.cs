4b3958c7de052582d69ef342b9e3a7273f83e5e9
SeYMeN991 <seymenh.99@gmail.com>
2025-05-18 20:38:45 +0300

    health pickup script inproved

--- Assets/Scripts/HealthPickupScript.cs
+++ Assets/Scripts/HealthPickupScript.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class HealthPickupScript : MonoBehaviour
 {
-    public GameObject healtPickupPrefab;
+    public GameObject healthPickupPrefab;
 
     public Vector2 spawnAreaMin = new Vector2(-100, -100);
 
     public Vector2 spawnAreaMax = new Vector2(100, 100);
 
-    public float spawnInterval = 100f; 
+    public float spawnInterval = 10f;
 
     public int healAmount = 20;
 
     void Start()
     {
+        spawnInterval = Random.Range(10, 30);
         InvokeRepeating(nameof(SpawnHealthPickups), 10f, spawnInterval);
     }
 
     public void SpawnHealthPickups()
     {
+        int currentPickupCount = GameObject.FindGameObjectsWithTag("HealthPickup").Length;
+        if (currentPickupCount >= 5)
+        {
+            return;
+        }
+
         Vector2 randomPosition = new Vector2(
             Random.Range(spawnAreaMin.x, spawnAreaMax.x),
             Random.Range(spawnAreaMin.y, spawnAreaMax.y)
         );
 
-        Instantiate(healtPickupPrefab, randomPosition, Quaternion.identity);
+        Instantiate(healthPickupPrefab, randomPosition, Quaternion.identity);
     }
 
     public void OnTriggerEnter2D(Collider2D other)
